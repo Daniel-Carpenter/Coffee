643d5b3028d7bc6dad393d9bc754bf58fea46039
Daniel Carpenter <dbry1024@gmail.com>
2020-04-19 13:11:58 -0500

    MS XL format

--- Analysis/Google Sheets Pull.R
+++ Analysis/Google Sheets Pull.R
@@ -6,12 +6,12 @@ library(googlesheets4)
 
 
 #main       <-  read_sheet("https://docs.google.com/spreadsheets/d/1fPaFaDq9XK6PiDpGK2qx-Z9yODEWRCmsXc2vfj4VSTQ/edit#gid=1764921743",
-main        <-  read.csv("C:\Users\dbry1\Google Drive\Personal G.Forms\Coffee\R Analysis - Copy Data",
+main        <-  read.csv("Analysis/Data from G Drive/Coffee Analysis - Data.csv",
                             sheet = "Data") %>%
                             unite(helper, Date, 'Total Seconds')
 
 #brewMaster <-  read_sheet("https://docs.google.com/spreadsheets/d/1d5VpUbqruQvQ95_BI1FW2vM3iAUUPnJQXVBOnpltPUI/edit#gid=1705883784",
-brewMaster  <-  read.csv("C:\Users\dbry1\Google Drive\Personal G.Forms\Coffee\R Analysis - Copy Data",
+brewMaster  <-  read.csv("Analysis/Data from G Drive/Untitled - Untitled.csv",
                             sheet = "Untitled") %>% 
                             mutate(date  = as.Date(createdAt)) %>%
                             unite(helper, date, 'total time')
